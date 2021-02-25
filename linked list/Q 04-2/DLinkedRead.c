0bebeaacbb290918dcbd671ccf435ac0f4c157ad
punpon9723 <punpon53@naver.com>
2021-02-25 21:50:20 +0900

    더미 노드 예제

---
+++ linked list/Q 04-2/DLinkedRead.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+	int data;
+	struct node* next;
+} Node;
+
+int main(void)
+{
+	Node* head=NULL;
+	Node* tail=NULL;
+	Node* cur=NULL;
+
+	Node * newNode=NULL;
+	int readData;
+	
+	head=(Node*)malloc(sizeof(Node));
+	tail=head;
+	//데이터를 입력 받는 과정
+	while(1)
+	{
+		printf("자연수 입력: ");
+		scanf("%d",&readData);
+		if(readData<1)
+			break;
+
+		//노드의 추가과정
+		newNode=(Node*)malloc(sizeof(Node));
+		newNode->data=readData;
+		newNode->next=NULL;
+/*
+		if(head==NULL)
+			head=newNode;
+		else
+			tail->next=newNode;
+*/
+		tail->next=newNode;
+		tail=newNode;
+	}
+	printf("\n");
+
+	//입력 받은 데이터의 출력과정
+	printf("입력 받은 데이터의 전체출력! \n");
+	if(head==tail)
+	{
+		printf("저장된 자연수가 존재하지 않습니다. \n");
+	}
+	else
+	{
+		cur=head;
+	//	printf("%d ",cur->data);
+
+		while(cur->next!=NULL)
+		{
+			cur=cur->next;
+			printf("%d ", cur->data);
+		}
+	}
+	printf("\n\n");
+
+	//메모리의 해제과정
+	if(head==tail)
+	{
+		return 0;
+	}
+	else
+	{
+		Node* delNode=head;
+		Node* delNextNode=head->next;
+		//printf("%d을 삭제합니다. \n", head->data);
+		//free(delNode);
+
+		while(delNextNode!=NULL)
+		{
+			delNode=delNextNode;
+			delNextNode=delNextNode->next;
+
+			printf("%d을 삭제합니다. \n",delNode->data);
+			free(delNode);
+		}
+	}
+	return 0;
+}
